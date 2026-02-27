93fc01e57f8e46f0726577b34bd0b09a42866d5d
Charles Iliya Krempeaux <iamreiver@gmail.com>
2026-02-26 16:21:45 -0800

    dial timeout

--- srv/db/conn.go
+++ srv/db/conn.go
@@ -2,6 +2,7 @@ package dbsrv
 
 import (
 	"fmt"
+	"time"
 
 	"github.com/ClickHouse/clickhouse-go/v2"
 	"github.com/ClickHouse/clickhouse-go/v2/lib/driver"
@@ -23,6 +24,7 @@ func init() {
 			Username: cfg.ClickHouseUserName(),
 			Password: cfg.ClickHousePassWord(),
 		},
+		DialTimeout: 30 * time.Second,
 	})
 	if nil != err {
 		panic(fmt.Errorf("failed to connect to ClickHouse database server (%q): %w", host, err))
